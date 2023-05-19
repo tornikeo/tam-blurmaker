dd3577eba3c689ae230c9c52b29e3fda8b3f944b
tornikeo <tornikeonoprishvili@gmail.com>
2023-05-19 10:41:08 +0400

    Fix video not generating

--- run-gui.sh
+++ run-gui.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-DOCKER_BUILTKIT=1 docker compose build && docker compose run -it -p 6080:6080 --rm  app python app.py --device cpu --sam_model_type vit_b --debug
+DOCKER_BUILTKIT=1 docker compose build && docker compose run -it -p 6080:6080 -v .:/workdir --rm   app python app.py --device cpu --sam_model_type vit_b --debug
